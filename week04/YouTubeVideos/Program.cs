fb466c02051241be407cf504253b64fd5240e5e2
git config --global user.name Smenesesc <santiagomenesescrsj@gmail.com>
2025-01-29 20:33:34 -0600

    updated

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,52 +1,10 @@
 using System;
 using System.Collections.Generic;
 
-public class Comment
-{
-    public string Author { get; set; }
-    public string Text { get; set; }
-
-    public Comment(string author, string text)
-    {
-        Author = author;
-        Text = text;
-    }
-}
-
-public class Video
-{
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public int LengthInSeconds { get; set; }
-    public List<Comment> Comments { get; set; }
-
-    public Video(string title, string author, int lengthInSeconds)
-    {
-        Title = title;
-        Author = author;
-        LengthInSeconds = lengthInSeconds;
-        Comments = new List<Comment>();
-    }
-
-    // This just tells you how many comments this vid has
-    public int GetCommentCount()
-    {
-        return Comments.Count;
-    }
-
-    // Adds a new comment to the list
-    public void AddComment(Comment comment)
-    {
-        Comments.Add(comment);
-    }
-}
-
 class Program
 {
     static void Main(string[] args)
     {
-
-        // Comments
         Comment comment1 = new Comment("moolah", "Yo, this video is dope!");
         Comment comment2 = new Comment("EpicBob", "Totally learned something new, thanks!");
         Comment comment3 = new Comment("DRmanco", "Super chill , loved it!");
@@ -54,12 +12,10 @@ class Program
         Comment comment5 = new Comment("azyd", "Ggs great gameplay");
         Comment comment6 = new Comment("Cazzette", "For real, this was goated!");
 
-        // videos
         Video video1 = new Video("Levi vs GM", "Gothamchess", 360);
         Video video2 = new Video("Warzone Gameplay", "bifl", 300);
         Video video3 = new Video("Best Messi Goals", "MagicMessi", 420);
 
-        // Adding the comments
         video1.AddComment(comment1);
         video1.AddComment(comment2);
         video1.AddComment(comment3);
@@ -72,7 +28,6 @@ class Program
         video3.AddComment(comment3);
         video3.AddComment(comment5);
 
-        // store the videos in a list
         List<Video> videos = new List<Video> { video1, video2, video3 };
         foreach (Video video in videos)
         {
