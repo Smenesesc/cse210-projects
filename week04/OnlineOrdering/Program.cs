84857365a99bf4b19053cbd348e1ecef9c7db687
git config --global user.name Smenesesc <santiagomenesescrsj@gmail.com>
2025-01-29 21:30:37 -0600

    updated

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("Urbanizacion El Cedral", "San Pedro", "San Jose", "Costa Rica");
+        Address address2 = new Address("Villas de Tulin", "San Pedro", "San Jose", "Costa Rica");
+
+        Customer customer1 = new Customer("Santiago Meneses", address1);
+        Customer customer2 = new Customer("Sebastian Hidalgo", address2);
+
+        Product product1 = new Product("Laptop", 101, 899.99, 1);
+        Product product2 = new Product("Headphones", 102, 199.99, 2);
+        Product product3 = new Product("Mouse", 103, 49.99, 3);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product3);
+
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.CalculateTotalCost():0.00}\n");
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.CalculateTotalCost():0.00}");
     }
 }
